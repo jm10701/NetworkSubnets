45f5290cde8865ecb737a3c61a3bc7455bbd966e
Juan Carlos Murillo <jm2439@cornell.edu>
2025-03-19 23:53:52 -0400

    Added solutons

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 73867
+
+#problem 1
+p1Network = '173.207.217.32'
+p1Broadcast = '173.207.217.39'
+p1First = '173.207.217.33'
+p1Last =  '173.207.217.38'
+
+#problem 2
+p2Network = '84.192.179.64'
+p2Broadcast = '84.192.179.127'
+p2First = '84.192.179.65'
+p2Last = '84.192.179.126'
+
+#problem 3
+p3Network = '251.177.140.0'
+p3Broadcast = '251.177.140.255'
+p3First = '251.177.140.1'
+p3Last = '251.177.140.254'
+
+#problem 4
+p4Network = '162.162.102.160'
+p4Broadcast = '162.162.102.191'
+p4First = '162.162.102.161'
+p4Last = '162.162.102.190'
+
+#problem 5
+p5Network = '73.147.64.0'
+p5Broadcast = '73.147.64.255'
+p5First = '73.147.64.1'
+p5Last = '73.147.64.254'
+
+#problem 6
+p6Network = '240.132.26.248'
+p6Broadcast = '240.132.26.255'
+p6First = '240.132.26.249'
+p6Last =  '240.132.26.254'
+
+#problem 7
+p7Network = '151.118.243.35'
+p7Broadcast = '151.118.243.127'
+p7First = '151.118.243.1'
+p7Last = '151.118.243.126'
+
+#problem 8
+p8Network = '62.103.205.72'
+p8Broadcast = '62.103.205.79'
+p8First = '62.103.205.73'
+p8Last = '62.103.205.78'
+
+#problem 9
+p9Network = '229.88.167.64'
+p9Broadcast = '229.88.167.127'
+p9First = '229.88.167.65'
+p9Last = '229.88.167.126'
+
+#problem 10
+p10Network = '140.73.129.0'
+p10Broadcast = '140.73.129.255'
+p10First = '140.73.129.1'
+p10Last = '140.73.129.254'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
